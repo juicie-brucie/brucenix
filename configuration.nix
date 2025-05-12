523dc065bd75a8926586d4e6787630bbd25f0520
juicie_brucie <brucemackay17@proton.me>
2025-05-12 15:41:51 -0700

    fuck

--- configuration.nix
+++ configuration.nix
@@ -79,12 +79,10 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      curl
-     emacs
      git
      nettools
      rustdesk
      kitty
-     emacsPackages.openfoam
      paraview
      pciutils
    ];
